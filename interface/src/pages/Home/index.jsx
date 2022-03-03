7b41a9246abd5d1042219773f32b58cf096280d2
Eduardo <eduardoalvessousa031@gmail.com>
2022-03-03 00:02:19 -0300

    criação de telas

--- interface/src/pages/Home/index.jsx
+++ interface/src/pages/Home/index.jsx
@@ -6,8 +6,11 @@ export const Home = () => {
   return (
     <div className="container">
       <div className="containers"></div>
-      <div className="images">    
+      <div className="images">  
+      <button className="log">Login</button>  
         <span className="images-img">
+          <h1>AGENDA <br /> FUTEBOL</h1><br />
+          <p>PARA MANTER-SE SEMPRE ATUALIZADO DE TODOS OS JOGOS DE SEUS CAMPEONATOS</p>
           <img src={soccerIMG} alt="" />
         </span>
       </div>
