3cfc4db35cc19860a5e099dff7601308b5472c92
Eduardo <eduardoalvessousa031@gmail.com>
2022-03-06 00:02:59 -0300

    Resolvido bugs e criação da tela de jogos

---
+++ interface/src/pages/Jogo/index.jsx
@@ -0,0 +1,20 @@
+import "./style.css";
+
+export const Jogo = () => {
+  return (
+    <div className="container">
+      <div className="container-card">
+        <div className="txt">
+          <input type="text" />
+          <br />
+          <input type="text" name="" id="" />
+          <br />
+          <input type="text" name="" id="" />
+          <br />
+        </div>
+
+        <button className="del">Deletar</button>
+      </div>
+    </div>
+  );
+};
--- interface/src/pages/routes/index.jsx
+++ interface/src/pages/routes/index.jsx
@@ -2,6 +2,7 @@ import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import { Home } from '../Home'
 import { Login } from '../Login'
 import { Register } from '../Register'
+import { Jogo } from '../Jogo'
 
 export const AppRouter = () => {
     return(
@@ -10,6 +11,7 @@ export const AppRouter = () => {
                 <Route path='/login' exact element={<Login />} />
                 <Route path='/register' exact element={<Register />} />
                 <Route path='/home' exact element={<Home />} />
+                <Route path='/jogo' exact element={<Jogo />} />
             </Routes>
         </Router>
     )
