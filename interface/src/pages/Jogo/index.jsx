9af7659569f51a01e2ad7efa4b1f06b98be8d2d4
Eduardo <eduardoalvessousa031@gmail.com>
2022-03-06 14:38:25 -0300

    tela de jogos

--- interface/src/pages/Jogo/index.jsx
+++ interface/src/pages/Jogo/index.jsx
@@ -1,19 +1,31 @@
+import { FaFutbol } from "react-icons/fa";
+import { FaPen } from "react-icons/fa";
+
+//import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import "./style.css";
 
 export const Jogo = () => {
   return (
     <div className="container">
+      <FaFutbol className="fut"></FaFutbol>
+      <FaFutbol className="fut2"></FaFutbol>
+
       <div className="container-card">
         <div className="txt">
-          <input type="text" />
           <br />
-          <input type="text" name="" id="" />
+      
+          <input type="text" name="" id="caixa" placeholder="Horário" />    <button className="penn">
+            <FaPen className="pen"></FaPen>
+          </button>
           <br />
-          <input type="text" name="" id="" />
           <br />
+          <input type="text" name="" id="caixa" placeholder="Local" />
+          <br />
+          <br />
+          <input type="text" name="" id="caixa" placeholder="Data" />
+          <br />
+          <button className="del">Deletar</button>
         </div>
-
-        <button className="del">Deletar</button>
       </div>
     </div>
   );
