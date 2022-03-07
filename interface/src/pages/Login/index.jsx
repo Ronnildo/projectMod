430aac51f572c0066c83c032140d680338611ece
Ronnildo <ronildosp03@gmail.com>
2022-03-07 02:35:27 -0300

    Consumo api login

--- interface/src/pages/Login/index.jsx
+++ interface/src/pages/Login/index.jsx
@@ -1,55 +1,87 @@
 import { Link } from "react-router-dom";
 import { useState } from "react";
 import { LayoutComponents } from "../../components/LayoutComponents";
+import React from "react";
+import axios from "axios";
 
-export const Login = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+export default class Login extends React.Component {
+  state = {
+    email: "",
+    password: "",
+  };
 
-  return (
-    <LayoutComponents>
-      <form className="login-form">
-        {/*classe para estilo do no login */}
-        <span className="login-form-title">login</span>
-        <br />
-        <br />
-        {/*div para as caixas de texto*/}
-        <div className="wrap-input">
-          {/*se o valor for diferente de vazil vou adicionar has-val, se estiver vazil permaneça com o input*/}
-          <input
-            className={email !== "" ? "has-val input" : "input"}
-            type="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-          <span className="focus-input" data-placeholder="Email"></span>
-        </div>
+  handleSingIn = async (e) => {
+    e.preventDefault();
 
-        <div className="wrap-input">
-          {/*se o valor for diferente de vazil vou adicionar has-val, se estiver vazil permaneça com o input*/}
-          <input
-            className={password !== "" ? "has-val input" : "input"}
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-          <span className="focus-input" data-placeholder="Passaword"></span>
-        </div>
+    let url = "http://localhost:3333/login";
 
-        {/*div para o botão*/}
-        <div className="container-login-form-btn">
-          <button className="login-form-btn">Login</button>
-        </div>
+    const data = {
+      email: this.state.email,
+      password: this.state.password,
+    };
+    axios
+      .post(url, data)
+      .then((res) => {
+        console.log(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
-        {/*div (não possui conta?)*/}
-        <div className="text">
-          <span className="text1">Não possui conta?</span>
+  handleChangeEmail = (event) => {
+    this.setState({ email: event.target.value });
+  };
+  handleChangePassword = (event) => {
+    this.setState({ password: event.target.value });
+  };
 
-          <Link className="text2" to="/register">
-            Criar conta.
-          </Link>
-        </div>
-      </form>
-    </LayoutComponents>
-  );
-};
+  render() {
+    return (
+      <LayoutComponents>
+        <form onSubmit={this.handleSingIn} className="login-form">
+          {/*classe para estilo do no login */}
+          <span className="login-form-title">login</span>
+          <br />
+          <br />
+          {/*div para as caixas de texto*/}
+          <div className="wrap-input">
+            {/*se o valor for diferente de vazil vou adicionar has-val, se estiver vazil permaneça com o input*/}
+            <input
+              //className={email !== "" ? "has-val input" : "input"}
+              type="email"
+              //value={"email"}
+              onChange={this.handleChangeEmail}
+            />
+            <span className="focus-input" data-placeholder="Email"></span>
+          </div>
+
+          <div className="wrap-input">
+            {/*se o valor for diferente de vazil vou adicionar has-val, se estiver vazil permaneça com o input*/}
+            <input
+              //className={password !== "" ? "has-val input" : "input"}
+              type="password"
+              //value={"password"}
+              onChange={this.handleChangePassword}
+            />
+            <span className="focus-input" data-placeholder="Passaword"></span>
+          </div>
+
+          {/*div para o botão*/}
+          <div className="container-login-form-btn">
+            <button className="login-form-btn">Login</button>
+          </div>
+
+          {/*div (não possui conta?)*/}
+          <div className="text">
+            <span className="text1">Não possui conta?</span>
+
+            <Link className="text2" to="/register">
+              Criar conta.
+            </Link>
+          </div>
+        </form>
+      </LayoutComponents>
+    );
+  }
+}
--- interface/src/pages/routes/index.jsx
+++ interface/src/pages/routes/index.jsx
@@ -1,20 +1,20 @@
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
-import { Home } from '../Home'
-import { Login } from '../Login'
-import  Register  from '../Register'
-import { Jogo } from '../Jogo'
-import { Cadast } from '../Cadast'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Home } from "../Home";
+import Login from "../Login";
+import Register from "../Register";
+import { Jogo } from "../Jogo";
+import { Cadast } from "../Cadast";
 
 export const AppRouter = () => {
-    return(
-        <Router>
-            <Routes>
-                <Route path='/login' exact element={<Login />} />
-                <Route path='/register' exact element={<Register />} />
-                <Route path='/home' exact element={<Home />} />
-                <Route path='/jogo' exact element={<Jogo />} />
-                <Route path='/cadast' exact element={<Cadast />} />
-            </Routes>
-        </Router>
-    )
-}
+  return (
+    <Router>
+      <Routes>
+        <Route path="/login" exact element={<Login />} />
+        <Route path="/register" exact element={<Register />} />
+        <Route path="/home" exact element={<Home />} />
+        <Route path="/jogo" exact element={<Jogo />} />
+        <Route path="/cadast" exact element={<Cadast />} />
+      </Routes>
+    </Router>
+  );
+};
