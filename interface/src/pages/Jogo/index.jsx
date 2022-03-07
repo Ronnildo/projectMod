29d786ef9f5f642009f7f312dfd098bd4d5dcfed
Eduardo <eduardoalvessousa031@gmail.com>
2022-03-07 02:44:12 -0300

    api

--- interface/src/pages/Jogo/index.jsx
+++ interface/src/pages/Jogo/index.jsx
@@ -5,11 +5,62 @@ import { Link } from "react-router-dom";
 //import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import "./style.css";
 import React from "react";
+import axios from "axios";
 
 export default class Jogo extends React.Component {
+
+  state = {
+    casa: "",
+    fora: "",
+    horario: "",
+    local: "",
+    data: "",
+  };
+
+  handleSubmit = async (event) => {
+
+    let url = "http://localhost:3333/users";
+
+    const data = {
+      casa: this.state.casa,
+      fora: this.state.fora,
+      horario: this.state.horario,
+      local: this.state.local,
+      data: this.state.data
+    };
+
+    console.log(data);
+    axios
+      .post(url, data)
+      .then((res) => {
+        console.log(res);
+        console.log(res.data);
+      })
+      .catch((err) => {
+        alert("Email já cadastrado!");
+        console.log(err);
+      });
+  }
+
+  handleChangeCasa = (event) => {
+    this.setState({ casa: event.target.value });
+  };
+  handleChangeFora = (event) => {
+    this.setState({ fora: event.target.value });
+  };
+  handleChangeHorario = (event) => {
+    this.setState({ horario: event.target.value });
+  };
+  handleChangeLocal = (event) => {
+    this.setState({ local: event.target.value });
+  };
+  handleChangeData = (event) => {
+    this.setState({ data: event.target.value });
+  };
+
   render() {
     return (
-      <div>
+      <form onSubmit={this.handleSubmit} className="">
         <span className="spa"></span>
 
         <Link className="" to="/home">
@@ -21,10 +72,10 @@ export default class Jogo extends React.Component {
           <FaFutbol className="fut2"></FaFutbol>
           <p>X</p>
           <div className="time1">
-            <input type="text" name="" id="caixaa" placeholder="casa" />
+            <input type="text" name="" id="casa" placeholder="casa" />
           </div>
           <div className="time2">
-            <input type="text" name="" id="caixaa" placeholder="fora" />
+            <input type="text" name="" id="fora" placeholder="fora" />
           </div>
 
           <div className="container-card">
@@ -32,7 +83,7 @@ export default class Jogo extends React.Component {
               <br />
 
               <div className="caixa1">
-                <input type="text" name="" id="caixa" placeholder="Horário" />
+                <input type="text" name="" id="horario" placeholder="Horário" />
                 <button className="penn">
                   <FaPen className="pen"></FaPen>
                 </button>
@@ -40,7 +91,7 @@ export default class Jogo extends React.Component {
               <br />
 
               <div className="caixa2">
-                <input type="text" name="" id="caixa" placeholder="Local" />
+                <input type="text" name="" id="local" placeholder="Local" />
                 <button className="penn">
                   <FaPen className="pen"></FaPen>
                 </button>
@@ -48,7 +99,7 @@ export default class Jogo extends React.Component {
               <br />
 
               <div className="caixa3">
-                <input type="text" name="" id="caixa" placeholder="Data" />
+                <input type="text" name="" id="data" placeholder="Data" />
                 <button className="penn">
                   <FaPen className="pen"></FaPen>
                 </button>
@@ -63,7 +114,7 @@ export default class Jogo extends React.Component {
 
           <span></span>
         </div>
-      </div>
+      </form>
     );
   }
 }
